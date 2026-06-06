ab113defdba0de42432f7bce38c0cf449170ffda
karpekin1991-pixel <karpekin1991@gmail.com>
2026-06-06 10:46:39 +0300

    Delete src directory

--- src/index.js
+++
@@ -1,8 +0,0 @@
-export default {
-  async fetch(request, env, ctx) {
-    const url = new URL(request.url);
-
-    // Serve static assets from /public via Assets binding
-    return env.ASSETS.fetch(request);
-  },
-};
